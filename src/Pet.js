fbfe7d10111663c4863661d7541dcdd33276b156
mourasoft <49453389+mourasoft@users.noreply.github.com>
2022-01-25 16:52:08 +0100

    edit pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,20 +1,21 @@
 import React from "react";
-
-// const Pet = (props) => {
-// 	return React.createElement("div", {}, [
-// 		React.createElement("h2", {}, props.name),
-// 		React.createElement("h2", {}, props.animal),
-// 		React.createElement("h2", {}, props.breed),
-// 	]);
-// };
-
-const Pet =(props) =>{
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <h3>{props.animal}</h3>
-            <h3>{props.breed}</h3>
-        </div>
-    )
-}
-export default Pet
+const Pet = ({ name, animal, breed, images, location, id }) => {
+	let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+	if (images.length) {
+		hero = images[0];
+	}
+	return (
+		<a href={`/details/${id}`} className="pet">
+			<div className="image-container">
+				<img src={hero} alt={name} />
+			</div>
+            <div className="info">
+                     <h2>{name}</h2>
+                     <h3>{`${animal} - ${breed} - ${location}`}</h3>
+                     
+                 </div>
+		</a>
+	);
+	// )
+};
+export default Pet;
